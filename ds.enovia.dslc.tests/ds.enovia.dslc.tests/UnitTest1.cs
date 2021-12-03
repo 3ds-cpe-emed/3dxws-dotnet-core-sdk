acbe94c84f834bfb8fd004af88bdeeed7af3e1b1
AAA27 <antonio.nogueira@3ds.com>
2021-12-03 18:59:26 +0100

    add: dslc tests project

---
+++ ds.enovia.dslc.tests/ds.enovia.dslc.tests/UnitTest1.cs
@@ -0,0 +1,130 @@
+using ds.authentication;
+using ds.authentication.redirection;
+using ds.enovia.common.collection;
+using ds.enovia.common.search;
+using ds.enovia.dseng.model;
+using ds.enovia.dseng.service;
+using ds.enovia.dslc.model;
+using ds.enovia.dslc.service;
+using ds.enovia.model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ds.enovia.dslc.tests
+{
+    public class Tests
+    {
+        const string DS3DXWS_AUTH_USERNAME = "DS3DXWS_AUTH_USERNAME";
+        const string DS3DXWS_AUTH_PASSWORD = "DS3DXWS_AUTH_PASSWORD";
+        const string DS3DXWS_AUTH_PASSPORT = "DS3DXWS_AUTH_PASSPORT";
+        const string DS3DXWS_AUTH_ENOVIA   = "DS3DXWS_AUTH_ENOVIA";
+        const string DS3DXWS_AUTH_TENANT   = "DS3DXWS_AUTH_TENANT";
+
+        string m_username    = string.Empty;
+        string m_password    = string.Empty;
+        string m_passportUrl = string.Empty;
+        string m_enoviaUrl   = string.Empty;
+        string m_tenant      = string.Empty;
+
+        UserInfo m_userInfo = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_username    = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_USERNAME, EnvironmentVariableTarget.User); // e.g. AAA27
+            m_password    = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_PASSWORD, EnvironmentVariableTarget.User); // e.g. your password
+            m_passportUrl = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_PASSPORT, EnvironmentVariableTarget.User); // e.g. https://eu1-ds-iam.3dexperience.3ds.com:443/3DPassport
+
+            m_enoviaUrl   = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_ENOVIA, EnvironmentVariableTarget.User); // e.g. https://r1132100982379-eu1-space.3dexperience.3ds.com:443/enovia
+            m_tenant      = Environment.GetEnvironmentVariable(DS3DXWS_AUTH_TENANT, EnvironmentVariableTarget.User); // e.g. R1132100982379
+        }
+
+        public async Task<IPassportAuthentication> Authenticate()
+        {
+            UserPassport passport = new UserPassport(m_passportUrl);
+
+            UserInfoRedirection userInfoRedirection = new UserInfoRedirection(m_enoviaUrl, m_tenant);
+            userInfoRedirection.Current = true;
+            userInfoRedirection.IncludeCollaborativeSpaces = true;
+            userInfoRedirection.IncludePreferredCredentials = true;
+
+            m_userInfo = await passport.CASLoginWithRedirection<UserInfo>(m_username, m_password, false, userInfoRedirection);
+
+            Assert.IsNotNull(m_userInfo);
+
+            StringAssert.AreEqualIgnoringCase(m_userInfo.name, m_username);
+
+            Assert.IsTrue(passport.IsCookieAuthenticated);
+
+            return passport;
+        }
+
+        public BusinessObjectData GetBusinessObjectData(string _physicalId, string _type, string _relativePath)
+        {
+            BusinessObjectDualIdentifier boId = new BusinessObjectDualIdentifier();
+            boId.id = _physicalId;
+            boId.identifier = _physicalId;
+            //Note : It was verified that the source property of the input should be null (R2022XGA) this might change in future versions
+            //boId.source = m_enoviaUrl; "$3DSpace";
+            boId.type = _type;
+            boId.relativePath = _relativePath;
+
+            BusinessObjectData __boData = new BusinessObjectData();
+            __boData.data = new List<BusinessObjectDualIdentifier>();
+            __boData.data.Add(boId);
+
+            return __boData;
+        }
+
+        [TestCase("VPLMAdmin.Company Name.Default", "prd-R1132100982379-00342846", "C.1" )]
+        //[TestCase("VPLMAdmin.Company Name.Default", "prd-R1132100982379-00355456", "A.1")]
+        public async Task Exercise_GetGraphVersion(string _securityContext, string _name, string _revision)
+        {
+            #region Arrange
+            //Authenticate
+            IPassportAuthentication passport = await Authenticate();
+
+            //Search for an Engineering product
+            EngineeringServices engineeringServices = new EngineeringServices(m_enoviaUrl, passport);
+            engineeringServices.SecurityContext = _securityContext;
+            engineeringServices.Tenant = m_tenant;
+
+            SearchByNameRevision searchByNameRevision = new SearchByNameRevision(_name, _revision);
+
+            NlsLabeledItemSet<EngineeringItem> engItems = await engineeringServices.Search(searchByNameRevision);
+
+            Assert.IsNotNull(engItems);
+
+            Assert.AreEqual(engItems.totalItems, 1);
+
+            EngineeringItem engItem = engItems.member[0];
+
+            Assert.IsNotNull(engItem);
+
+            string pid  = engItem.id;
+            string type = engItem.type;
+            string relativePath = $"/resources/v1/modeler/dseng/dseng:EngItem/{pid}";
+
+            //Prepare Data
+            BusinessObjectData data = GetBusinessObjectData(pid, type, relativePath);
+
+            //Act - Initialize Collaborative Lifecycle Services
+            CollaborativeLifecycleService lifecycleService = new CollaborativeLifecycleService(m_enoviaUrl, passport);
+
+            lifecycleService.SecurityContext = _securityContext;
+            lifecycleService.Tenant = m_tenant;
+            #endregion
+            
+            #region Act
+            //Call Collaborative Lifecycle Services Get Graph
+            VersionGraph versionGraph =  await lifecycleService.GetVersionGraph(data);
+            #endregion
+
+            #region Assert
+            Assert.IsNotNull(versionGraph);
+            #endregion
+        }
+    }
+}
